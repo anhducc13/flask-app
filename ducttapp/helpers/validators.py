d30d93d973f9b2566090353d426f1aed043eb0f2
anhducc13 <trantienduc10@gmail.com>
2019-07-18 15:12:47 +0700

    update

--- ducttapp/helpers/validators.py
+++ ducttapp/helpers/validators.py
@@ -1,10 +1,8 @@
 import re
-from uuid import UUID
 
-REGEX_USERNAME = r"(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,20})$"
-REGEX_PASSWORD = r"(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,20})$"
+REGEX_USERNAME = r"(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,128})$"
+REGEX_PASSWORD = r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,128}$"
 REGEX_EMAIL = r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-REGEX_UUIDV4 = r"^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$"
 
 
 def valid_username(value):
@@ -18,6 +16,3 @@ def valid_password(value):
 def valid_email(value):
     return re.match(REGEX_EMAIL, value)
 
-
-def valid_uuid(value):
-    return re.match(REGEX_UUIDV4, value, re.IGNORECASE)
--- ducttapp/services/auth/logout.py
+++ ducttapp/services/auth/logout.py
@@ -3,6 +3,7 @@ from ducttapp import repositories
 
 def logout(jti):
     repositories.revoked_token.save_revoked_token_to_database(jti=jti)
-    return {
-               'ok': True
-           }, 200
+    response = {
+        'ok': True
+    }
+    return response, 200
